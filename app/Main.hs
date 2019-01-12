a0bdadc4b47c3a9c40e43f9e30b18ee1e4fbbe27
chengzh2008 <>
2019-01-12 12:00:07 -0800

    basic command-driven cli setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,40 @@
 module Main where
 
-import Lib
+import           Data.Semigroup      ((<>))
+import           Lib
+import           Options.Applicative (ParserInfo, CommandFields, Mod, Parser, argument, command, execParser,
+                                      idm, info, str, subparser)
+import Control.Monad (join)
+
+type Host = String
+type Port = Integer
+
+data Command = Serve Host Port
+
+newtype Options = Options { cmd:: Command }
+
+-- | Start up the server and serve request
+server :: IO ()
+server = putStrLn "Server starting..."
+
+-- | CLI options parser
+opts :: Parser (IO ())
+opts = subparser commands
+  where
+    serverAction :: Parser (IO ())
+    serverAction = pure server
+
+    serverCmd :: ParserInfo (IO ())
+    serverCmd = info serverAction idm
+
+    commands :: Mod CommandFields (IO ())
+    commands = command "server" serverCmd
+
+
+
 
 main :: IO ()
-main = someFunc
+main = join $ execParser parser
+  where
+    parser :: ParserInfo (IO ())
+    parser = info opts idm
